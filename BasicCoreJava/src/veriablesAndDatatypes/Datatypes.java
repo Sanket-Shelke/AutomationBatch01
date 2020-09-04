634d3efe980ca6e67a512b16820b2c096e9bc30b
Sanket-Shelke <sanshelke18@gmail.com>
2020-09-04 15:58:46 +0530

    @sanket - Commiting first code

---
+++ BasicCoreJava/src/veriablesAndDatatypes/Datatypes.java
@@ -0,0 +1,15 @@
+package veriablesAndDatatypes;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 25; 
+		String name = "Sanket";
+		char gender = 'M';
+		boolean marritialStatus = true;
+		double height = 5.; 
+		System.out.println(age);
+	}
+
+}
